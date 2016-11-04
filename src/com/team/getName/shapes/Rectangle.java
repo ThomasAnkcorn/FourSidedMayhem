685cee7524d48ada41f1000113b399572bb88310
hlloyd <hlloyd64@googlemail.com>
2016-11-04 11:36:57 +0000

    Rectangle classes added

---
+++ src/com/team/getName/shapes/Rectangle.java
@@ -0,0 +1,19 @@
+package com.team.getName.shapes;
+
+public class Rectangle extends Parallelogram {
+
+		
+	public Rectangle(){
+		isRectangle();
+		
+	}
+	
+	private boolean isRectangle(){
+		return false;
+	}
+	
+	public double getArea(){
+		return 0;
+	}
+	
+}
