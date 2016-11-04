d4a8056b3c665b7ffa8a7c5f7fd40742bd14a9d0
samwdp <samwdp@gmail.com>
2016-11-04 11:18:26 +0000

    Packages and test

---
+++ src/com/team/getName/main/TestProgram.java
@@ -0,0 +1,10 @@
+package com.team.getName.main;
+
+public class TestProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/team/getName/test/PointTest.java
@@ -0,0 +1,14 @@
+package com.team.getName.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PointTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
