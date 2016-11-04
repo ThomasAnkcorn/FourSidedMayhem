cf893c14fbf9e0ca804559f91c43b6cb52405e99
Dannersjb <dannersjb@gmail.com>
2016-11-04 11:29:28 +0000

    creating the trapezium class and test class

---
+++ src/com/team/getName/shapes/Trapezium.java
@@ -0,0 +1,29 @@
+package com.team.getName.shapes;
+
+public class Trapezium {
+	private double longBase;
+	private double shortBase;
+	private double slantingSide1;
+	private double slantingSide2;
+	
+	public Trapezium(Object trap) {
+		//trap.longBase = this.longBase;
+		//trap.shortBase = this.shortBase;
+		//trap.slantingSide1 = this.slantingSide1;
+		//trap.slantingSide2 = this.slantingSide2;
+	}
+	
+	public static boolean isTrapezium() {
+		return false;
+	}
+	
+	private void calculate4Sides() {
+		
+	}
+	
+	public void getArea() {
+		
+	}
+	
+	
+}
