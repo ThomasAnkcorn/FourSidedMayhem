a95c9b5216fbce1abc70de856667cda8f1105e0e
ThomasAnkcorn <thomasankcorn@gmail.com>
2016-11-04 10:49:10 +0000

    Commiting Point to string could be wrong

---
+++ src/Point.java
@@ -0,0 +1,36 @@
+
+public class Point {
+
+	private double xpos;
+	private double ypos;
+	
+	Point(double xpos, double ypos){
+		this.xpos = xpos;
+		this.ypos = ypos;
+	}
+	public double getXpos() {
+		return xpos;
+	}
+	
+	
+	public void distanceTo(){
+		
+	}
+	
+	public void setXpos(double xpos) {
+		this.xpos = xpos;
+	}
+	public double getYpos() {
+		return ypos;
+	}
+	public void setYpos(double ypos) {
+		this.ypos = ypos;
+	}
+	
+	
+	public String toString(Point p){
+		return "A Point with x = "+p.xpos+" and y = "+p.ypos+"!";
+	}
+	
+	
+}
