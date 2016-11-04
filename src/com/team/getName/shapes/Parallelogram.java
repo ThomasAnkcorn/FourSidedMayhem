fc0db2d4967ccc330c51d6e987c1a8ec4f8618b7
samwdp <samwdp@gmail.com>
2016-11-04 14:35:31 +0000

    Parallelogram is finished

--- src/com/team/getName/shapes/Parallelogram.java
+++ src/com/team/getName/shapes/Parallelogram.java
@@ -3,18 +3,31 @@ package com.team.getName.shapes;
 public class Parallelogram extends Trapezium{
 
 	
-		private boolean isParallelogram(){
-			return true;
+		public static boolean isParallelogram(Quadrilateral quad){
+			
+			if(quad.getL1().getLength() == quad.getL3().getLength() && quad.getL1().getSlope() == quad.getL3().getSlope() && quad.getL1().getSlope() > 0 && quad.getL3().getSlope() > 0){
+				if(quad.getL2().getLength() == quad.getL4().getLength() && quad.getL2().getSlope() == quad.getL4().getSlope()){
+					return true;
+				}
+			} else{
+				return false;
+			}
+			return false;
 		}
 		
-		public Parallelogram() {
+		public Parallelogram(Point p1, Point p2, Point p3, Point p4) {
 			
-			super(null, null, null, null);
-			// TODO Auto-generated constructor stub
+			super(p1,p2,p3,p4);
+			
 		}
 
 
 		public double getArea(){
-			return 0;
+			
+			double height = getP2().getXpos() - getP1().getXpos();
+			System.out.println(height);
+			double length = getL2().getLength();
+			System.out.println(length);
+			return height * length;
 		}
 }
--- src/com/team/getName/test/ParallelogramTest.java
+++ src/com/team/getName/test/ParallelogramTest.java
@@ -4,11 +4,78 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.team.getName.shapes.Parallelogram;
+import com.team.getName.shapes.Point;
+
 public class ParallelogramTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testArea() {
+		
+		Point p1 = new Point(0.0, 0.0);
+		Point p2 = new Point(1.0,2.0);
+		Point p3 = new Point(1.0,5.0);
+		Point p4 = new Point(0.0,3.0);
+		
+		double expected = 3;
+		
+		
+		
+		Parallelogram p = new Parallelogram(p1, p2, p3, p4);
+		double actual = p.getArea();
+		
+		
+		assertEquals(expected, actual, 0.1);
+	}
+	
+	@Test
+	public void testIsParallelogram(){
+		Point p1 = new Point(0.0, 0.0);
+		Point p2 = new Point(1.0,2.0);
+		Point p3 = new Point(1.0,5.0);
+		Point p4 = new Point(0.0,3.0);
+		
+		boolean expected = true;
+		
+		
+		
+		Parallelogram p = new Parallelogram(p1, p2, p3, p4);
+		boolean actual = Parallelogram.isParallelogram(p);
+		
+		assertEquals(expected, actual);
 	}
 
+	@Test
+	public void testIsParallelogramFalse(){
+		Point p1 = new Point(0.0, 0.0);
+		Point p2 = new Point(0.0,1.0);
+		Point p3 = new Point(1.0,1.0);
+		Point p4 = new Point(0.0,1.0);
+		
+		boolean expected = false;
+		
+		
+		
+		Parallelogram p = new Parallelogram(p1, p2, p3, p4);
+		boolean actual = Parallelogram.isParallelogram(p);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testIsParallelogramFalseChangePos(){
+		Point p2 = new Point(0.0, 0.0);
+		Point p3 = new Point(0.0,1.0);
+		Point p4 = new Point(1.0,1.0);
+		Point p1 = new Point(0.0,1.0);
+		
+		boolean expected = false;
+		
+		
+		
+		Parallelogram p = new Parallelogram(p1, p2, p3, p4);
+		boolean actual = Parallelogram.isParallelogram(p);
+		
+		assertEquals(expected, actual);
+	}
 }
