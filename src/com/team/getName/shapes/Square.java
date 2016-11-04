e2db87d1fd362bfafa8a6ce27c9ffd239b37d4d8
ThomasAnkcorn <thomasankcorn@gmail.com>
2016-11-04 11:29:38 +0000

    Square

---
+++ src/com/team/getName/shapes/Square.java
@@ -0,0 +1,17 @@
+package com.team.getName.shapes;
+
+public class Square extends Rectangle{
+	
+	private boolean isSquare(){
+		return true;
+	}
+	
+	Square(){
+		
+	}
+	
+	public double getArea(){
+		return 0;
+	}
+
+}
