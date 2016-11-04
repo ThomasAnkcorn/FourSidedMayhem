643377cb3431b24b778657c6b0d26a76b1ff3be7
samwdp <samwdp@gmail.com>
2016-11-04 11:44:36 +0000

    Exception added

---
+++ src/com/team/getName/exception/ShapeException.java
@@ -0,0 +1,13 @@
+package com.team.getName.exception;
+
+public class ShapeException extends Exception{
+	
+	public ShapeException(){
+		
+	}
+	
+	public ShapeException(String message){
+		super(message);
+	}
+
+}
