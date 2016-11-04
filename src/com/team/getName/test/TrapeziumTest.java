8a4dfc88fdbbb8d572cecef3dbc3a44f14c6335d
Dannersjb <dannersjb@gmail.com>
2016-11-04 15:03:12 +0000

    trapezium tests + stuff

--- src/com/team/getName/test/TrapeziumTest.java
+++ src/com/team/getName/test/TrapeziumTest.java
@@ -13,11 +13,21 @@ public class TrapeziumTest {
 	public void getAreaTest() {
 		Trapezium t = new Trapezium(new Point(0,0), new Point(10,0), new Point(8,5), new Point(3.3, 5));
 		
-		double expected = 300;
+		double expected = 36.75;
 		double actual = t.getArea();
 		
 		assertEquals(expected, actual, 0.01);
 		
+	}
+	
+	@Test
+	public void isTrapeziumTest() {
+		Trapezium t = new Trapezium(new Point(0,0), new Point(10,0), new Point(8,5), new Point(3.3, 5));
+		
+		boolean expected = true;
+		boolean actual = t.isTrapezium(t);
+		
+		assertTrue(actual);
 	}
 
 }
